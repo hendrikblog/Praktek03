5f35d98f4c7e29bd08fecd81dd69ff3a6d158c30
hendrikblog <38091746+hendrikblog@users.noreply.github.com>
2018-05-06 00:06:52 +0800

    Method Dengan Nilai Balik

--- src/praktek03/Helm.java
+++ src/praktek03/Helm.java
@@ -4,12 +4,17 @@ public class Helm {
     String merk;
     String warna;
     double harga;
-    
+    double beli;
     void cetakInfo(){
         System.out.println("=====================");
         System.out.println("merk : "+merk);
         System.out.println("warna : "+warna);
         System.out.println("harga : "+harga);
         System.out.println("=====================");
+    }
+    double hitungjumlah(){
+        double jumlah;
+        jumlah=harga*beli;
+        return jumlah;
     }
 }
--- src/praktek03/HelmAksi.java
+++ src/praktek03/HelmAksi.java
@@ -6,8 +6,9 @@ public class HelmAksi {
         h1.merk = "kyt";
         h1.warna = "hitam";
         h1.harga = 200;
-        
+        h1.beli = 2;
         h1.cetakInfo();
+        System.out.println("jumlah Helm = "+h1.hitungjumlah());
         
     }
  
