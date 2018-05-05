21082be66987db4640b9f112bd18f6cc65266344
hendrikblog <38091746+hendrikblog@users.noreply.github.com>
2018-05-05 22:49:35 +0800

    Membuat Atribut Dan Behavior

--- src/praktek03/Helm.java
+++ src/praktek03/Helm.java
@@ -1,5 +1,15 @@
 
 package praktek03;
 public class Helm {
+    String merk;
+    String warna;
+    double harga;
     
+    void cetakInfo(){
+        System.out.println("=====================");
+        System.out.println("merk : "+merk);
+        System.out.println("warna : "+warna);
+        System.out.println("harga : "+harga);
+        System.out.println("=====================");
+    }
 }
