570f897c575b80919bf8e4066a8f0e42e36c8789
hendrikblog <38091746+hendrikblog@users.noreply.github.com>
2018-05-05 23:07:30 +0800

    Mengisi Nilai Atribute

--- src/praktek03/HelmAksi.java
+++ src/praktek03/HelmAksi.java
@@ -3,6 +3,10 @@ package praktek03;
 public class HelmAksi {
     public static void main(String[] args) {
         Helm h1= new Helm();
+        h1.merk = "kyt";
+        h1.warna = "hitam";
+        h1.harga = 200;
+        
     }
  
 }
